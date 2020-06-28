36be5cac8abc8bb979badd765ab78faa0d71d6fb
Willy <williamvn1995@gmail.com>
2020-06-28 14:37:55 +0200

    Links Added

--- js/script.js
+++ js/script.js
@@ -14,3 +14,19 @@ $(document).on("scroll", function(){
     }
     lastScrollTop = st <= 0 ? 0 : st;
 });
+var activeSection = $('.section.active');
+var activeSlide = activeSection.find('.slide.active');
+console.log(activeSlide);
+
+window.addEventListener('popstate', function(e){
+    sectionName = e.srcElement.$currentElement[0].baseURI.split("#")[1];
+    navbar = $('#navbar');
+    if(sectionName == "knowledgesLink" || sectionName == "experienceLink" || sectionName == "projectsLink"){
+        console.log(sectionName);
+        navbar.css("background-color", "black");
+    }
+    else{
+        console.log("No navbar color");
+        navbar.css("background", "none");
+    }
+});
